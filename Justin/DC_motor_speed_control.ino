7a1892ba9402c0933c3c6291162c6fadced9af71
JustinJRangad <108975956+JustinJRangad@users.noreply.github.com>
2023-09-05 21:27:38 +0530

    Update DC_motor_speed_control.ino

--- Justin/DC_motor_speed_control.ino
+++ Justin/DC_motor_speed_control.ino
@@ -1,134 +1,109 @@
-#include <util/atomic.h>
+#include <util/atomic.h> // For the ATOMIC_BLOCK macro
 
-// Pins
-#define ENCA 2
-#define ENCB 3
+#define ENCA 2 // YELLOW
+#define ENCB 3 // WHITE
 #define PWM 5
 #define IN1 6
-#define IN2 7
 
-// globals
+
+volatile int posi = 0;
 long prevT = 0;
-int posPrev = 0;
-// Use the "volatile" directive for variables
-// used in an interrupt
-volatile int pos_i = 0;
-volatile float velocity_i = 0;
-volatile long prevT_i = 0;
-
-float v1Filt = 0;
-float v1Prev = 0;
-float v2Filt = 0;
-float v2Prev = 0;
-
+float eprev = 0;
 float eintegral = 0;
 
 void setup() {
-  Serial.begin(115200);
-
+  Serial.begin(9600);
   pinMode(ENCA,INPUT);
   pinMode(ENCB,INPUT);
+  attachInterrupt(digitalPinToInterrupt(ENCA),readEncoder,RISING);
+  
   pinMode(PWM,OUTPUT);
   pinMode(IN1,OUTPUT);
-  pinMode(IN2,OUTPUT);
-
-  attachInterrupt(digitalPinToInterrupt(ENCA),
-                  readEncoder,RISING);
+ 
+  
+  Serial.println("target pos");
 }
 
 void loop() {
 
-  // read the position in an atomic block
-  // to avoid potential misreads
-  int pos = 0;
-  float velocity2 = 0;
-  ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
-    pos = pos_i;
-    velocity2 = velocity_i;
-  }
+  // set target position
+  //int target = 1200;
+  int target = 250*sin(prevT/1e6);
 
-  // Compute velocity with method 1
+  // PID constants
+  float kp = 1;
+  float kd = 0.025;
+  float ki = 0.0;
+
+  // time difference
   long currT = micros();
-  float deltaT = ((float) (currT-prevT))/1.0e6;
-  float velocity1 = (pos - posPrev)/deltaT;
-  posPrev = pos;
+  float deltaT = ((float) (currT - prevT))/( 1.0e6 );
   prevT = currT;
-
-  // Convert count/s to RPM
-  float v1 = velocity1/600.0*60.0;
-  float v2 = velocity2/600.0*60.0;
-
-  // Low-pass filter (25 Hz cutoff)
-  v1Filt = 0.854*v1Filt + 0.0728*v1 + 0.0728*v1Prev;
-  v1Prev = v1;
-  v2Filt = 0.854*v2Filt + 0.0728*v2 + 0.0728*v2Prev;
-  v2Prev = v2;
-
-  // Set a target
-  float vt = 100*(sin(currT/1e6)>0);
-
-  // Compute the control signal u
-  float kp = 5;
-  float ki = 10;
-  float e = vt-v1Filt;
-  eintegral = eintegral + e*deltaT;
-  
-  float u = kp*e + ki*eintegral;
-
-  // Set the motor speed and direction
-  int dir = 1;
-  if (u<0){
-    dir = -1;
+  int pos = 0; 
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+    pos = posi;
   }
-  int pwr = (int) fabs(u);
-  if(pwr > 255){
+  
+  // error
+  int e = pos - target;
+
+  // derivative
+  float dedt = (e-eprev)/(deltaT);
+
+  // integral
+  eintegral = eintegral + e*deltaT;
+
+  // control signal
+  float u = kp*e + kd*dedt + ki*eintegral;
+
+  // motor power
+  float pwr = fabs(u);
+  if( pwr > 255 ){
     pwr = 255;
   }
-  setMotor(dir,pwr,PWM,IN1,IN2);
 
-  Serial.print(vt);
+  // motor direction
+  int dir = 1;
+  if(u<0){
+    dir = -1;
+  }
+
+  // signal the motor
+  setMotor(dir,pwr,PWM,IN1);
+
+
+  // store previous error
+  eprev = e;
+
+  Serial.print(target);
   Serial.print(" ");
-  Serial.print(v1Filt);
+  Serial.print(pos);
   Serial.println();
-  delay(1);
 }
 
-void setMotor(int dir, int pwmVal, int pwm, int in1, int in2){
-  analogWrite(pwm,pwmVal); // Motor speed
-  if(dir == 1){ 
-    // Turn one way
+void setMotor(int dir, int pwmVal, int pwm, int in1){
+  analogWrite(pwm,pwmVal);
+  if(dir == 1){
     digitalWrite(in1,HIGH);
-    digitalWrite(in2,LOW);
+   
   }
   else if(dir == -1){
-    // Turn the other way
     digitalWrite(in1,LOW);
-    digitalWrite(in2,HIGH);
+    
   }
   else{
-    // Or dont turn
     digitalWrite(in1,LOW);
-    digitalWrite(in2,LOW);    
-  }
+    analogWrite(pwm,0);
+ 
+  }  
 }
 
 void readEncoder(){
-  // Read encoder B when ENCA rises
   int b = digitalRead(ENCB);
-  int increment = 0;
-  if(b>0){
-    // If B is high, increment forward
-    increment = 1;
+  if(b > 0){
+    posi++;
   }
   else{
-    // Otherwise, increment backward
-    increment = -1;
+    posi--;
   }
-  pos_i = pos_i + increment;
-
-  // Compute velocity with method 2
-  long currT = micros();
-  float deltaT = ((float) (currT - prevT_i))/1.0e6;
-  velocity_i = increment/deltaT;
-  prevT_i = currT;
 }
