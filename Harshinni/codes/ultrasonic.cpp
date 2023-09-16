e5c515a8449fdec89ab783469ff86388ccebfc9d
harshinni-babu <125139271+harshinni-babu@users.noreply.github.com>
2023-09-16 22:00:22 +0530

    Add files via upload

---
+++ Harshinni/codes/ultrasonic.cpp
@@ -0,0 +1,36 @@
+#define TRIG_PIN 
+#define ECHO_PIN 8
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+}
+
+void loop() {
+  long duration;
+  float distance_cm;
+
+  // Clear the trigger pin
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+
+  // Trigger the ultrasonic sensor
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  // Read the echo pulse duration
+  duration = pulseIn(ECHO_PIN, HIGH);
+
+  // Calculate distance in centimeters
+  distance_cm = duration * 0.034 / 2;
+
+  // Print the distance to the serial monitor
+  Serial.print("Distance: ");
+  Serial.print(distance_cm);
+  Serial.println(" cm");
+
+  delay(1000); // Wait for a moment before the next reading
+}
+
