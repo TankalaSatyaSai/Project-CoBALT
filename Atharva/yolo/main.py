fd99d39721aff9150d8689398288c774d3146563
Atharva <135647321+Error404Occured@users.noreply.github.com>
2024-02-01 20:45:41 +0530

    yolo model

---
+++ Atharva/yolo/main.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8n.yaml")  # build a new model from scratch
+model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+
+# Use the model
+model.train(data="data.yaml", epochs=500)  # train the model
+
